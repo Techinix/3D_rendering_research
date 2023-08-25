8b79a8c54de7dd4b7445272bb02c900876167b85
Techinix <zeko12800@gmail.com>
2023-08-25 16:06:05 +0100

    latest

--- nv_rd/__init__.py
+++ nv_rd/__init__.py
@@ -1,1 +1,6 @@
-from nv_rd.src.main import run_approach
+from .classic_nerf.run_classic_nerf import ClassicNerf
+from .mip_nerf.run_mip_nerf import MipNerf
+from .instant_ngp.run_instant_ngp_occ import InstantNgpOcc
+from .instant_ngp.run_instant_ngp_prop import InstantNgpProp
+from .main import run_approach
+
