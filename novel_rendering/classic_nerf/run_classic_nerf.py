237a5b99c162098d4f259588463e415430d87ad4
Techinix <zeko12800@gmail.com>
2023-08-28 15:30:53 +0100

    latest

--- novel_rendering/classic_nerf/run_classic_nerf.py
+++ novel_rendering/classic_nerf/run_classic_nerf.py
@@ -23,18 +23,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(levelname)s:     %(message)s'
     )
-"""def Argsparser():
-    parser=argparse.ArgumentParser(prog="Execution of Nerf Benchmarking",
-                                   description="An implementation of the Tiny Neural Radiance Field Nerf Approach\
-                                     to estimate 3D shapes from 2D images",
-                                    epilog="for further question, contact X@gmail.com",
-                                   )
-    
-    parser.add_argument("--config", help="Path to config file.", required=False, default='./configs/config.yaml')
-    parser.add_argument("opts", nargs=argparse.REMAINDER,
-                        help="Modify self.hparams Example: train.py resume out_dir TRAIN.BATCH_SIZE 2")
 
-    return parser"""
 
 
 class ClassicNerf():
